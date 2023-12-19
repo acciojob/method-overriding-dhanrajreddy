2a55ecc42fa44398a3ec1348f23398b1c86b80dd
dhanrajreddy <dhanrajkutwade7@gmail.com>
2023-12-19 23:49:09 +0530

    solved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,20 +1,10 @@
 package com.driver;
+import com.driver.test;
+import com.driver.test.TestCases;
 public class Main {
-    class A{
-    String meth(){
-        return "Invoking method from class A";
-    }
-}
-
-class B extends A{
-    String meth(){
-        super.meth();
-        return "Method is overridden in Extendend class B";
-    }
-
-}
+    
   public static void main(String[] args) {
-    A o =new B();
+     TestCases.A o =TestCases.new B();
     o.meth();
   }
 }
--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,8 +1,20 @@
 package com.driver.test;
-
+import com.driver.*;
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
+    class A{
+        String meth(){
+            return "Invoking method from class A";
+        }
+    }
     
+    class B extends A{
+        String meth(){
+            super.meth();
+            return "Method is overridden in Extendend class B";
+        }
+    
+    }
 }
